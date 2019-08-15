d259742f0e9f93d19f00dffa15cf88a75deab076
Modupe Adetunji <modupeore@gmail.com>
2019-08-15 12:52:12 -0400

    updated config.php

--- config.php
+++ config.php
@@ -5,5 +5,4 @@
   define('DB_PASSWORD', 'maryshelley');
   define('DB_DATABASE', 'transcriptatlas');
   $db_conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-  $basepath = '/home/modupe/public_html/newTAD/TransAtlasDB';
 ?>
